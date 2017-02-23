09ccb37db206e18d9e670c62389823e4440e950a
sulliel <solaiman.el.mahmoudi@gmail.com>
2017-02-23 23:39:04 +0100

    JobeetBundle and entities created

---
+++ src/Ens/JobeetBundle/Entity/CategoryAffiliate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ens\JobeetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CategoryAffiliate
+ *
+ * @ORM\Table(name="category_affiliate")
+ * @ORM\Entity(repositoryClass="Ens\JobeetBundle\Repository\CategoryAffiliateRepository")
+ */
+class CategoryAffiliate
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
---
+++ src/Ens/JobeetBundle/JobeetBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ens\JobeetBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JobeetBundle extends Bundle
+{
+}
